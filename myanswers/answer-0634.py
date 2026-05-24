a2910d1a17e313d2f95421f79a200919b8dba0e3
JorgeMT21 <jorge.marint@udea.edu.co>
2026-05-23 19:52:59 -0500

    Update answer-0634.py

--- myanswers/answer-0634.py
+++ myanswers/answer-0634.py
@@ -1,11 +1,11 @@
 from sklearn.metrics import precision_score
 
 def evaluar_precision_positiva(y_real, y_pred):
-    precision = precision_score(
-        y_real,
-        y_pred,
-        pos_label=1,
-        zero_division=0
+    return float(
+        precision_score(
+            y_real,
+            y_pred,
+            pos_label=1,
+            zero_division=0
+        )
     )
-
-    return float(precision)
